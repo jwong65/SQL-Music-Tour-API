7890fc1e714f27c6dd7ba99a964ce0547fe8a5fa
jwong65 <jason.wong6507@gmail.com>
2023-02-21 21:44:02 -0500

    Added the update route and delete route for testing

--- controllers/event_controller.js
+++ controllers/event_controller.js
@@ -37,4 +37,34 @@ events.post('/', async(req,res)=>{
 })
 
 //UPDATE ROUTE
+events.put('/:id', async(req, res)=>{
+    try{
+        const updatedEvent = await Event.update(req.body, {
+            where: {
+                event_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `This event has been updated ${updatedEvent}`
+        })
+    } catch (error){
+        res.status(500).json(error)
+    }
+})
+
+//DELETE ROUTE
+events.delete('/:id', async(req, res)=>{
+    try{
+        const deletedEvent = await Event.destroy({
+            where: {
+                event_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `This has been deleted, ${deletedEvent}`
+        })
+    }catch(error){
+        res.status(500).json(error)
+    }
+})
 module.exports = events
