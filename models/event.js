edbdb2533f3c97a84c07b7fdd09f3eece12f283e
jwong65 <jason.wong6507@gmail.com>
2023-02-23 18:43:02 -0500

    Added the event_id assoication between the event, meet_greet models

--- models/event.js
+++ models/event.js
@@ -7,12 +7,16 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({Stage, StageEvent}) {
+    static associate({Stage, StageEvent, MeetGreet}) {
       // define association here
       Event.belongsToMany( Stage, {
         foreignKey:'event_id',
         as: 'stages',
         through: StageEvent
+      })
+      Event.hasMany(MeetGreet, {
+        foreignKey: 'event_id',
+        as: 'meet_greets'
       })
     }
   }
--- models/meet_greet.js
+++ models/meet_greet.js
@@ -9,11 +9,15 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({Band}) {
+    static associate({Band, Event}) {
       // define association here
       meet_greet.belongsTo(Band,{
         foreignKey: 'band_id',
         as: 'band'
+      })
+      meet_greet.belongsTo(Event, {
+        foreignKey: 'event_id',
+        as: 'event'
       })
     }
   }
