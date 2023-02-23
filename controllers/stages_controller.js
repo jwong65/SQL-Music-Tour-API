d21379de8b53287830073227c512855646dd4f7c
jwong65 <jason.wong6507@gmail.com>
2023-02-23 10:48:27 -0500

    Fixed and added post for testing, no resulting message showing up on adding to index

--- controllers/stages_controller.js
+++ controllers/stages_controller.js
@@ -6,11 +6,24 @@ const { Stage } = db
 //INDEX
 stages.get('/', async(req, res)=>{
     try{
-        const foundStage = await Event.findAll()
+        const foundStage = await Stage.findAll()
         res.status(200).json(foundStage)
     }catch (error){
         res.status(500).json(error)
 }})
+
+//POST
+stages.post('/', async(req, res)=>{
+    try{
+    const newStage = await Stage.create(req.body)
+        res.status(200).json({
+            message: 'Successfully added a new Stage',
+            data: newEvent
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
 
 
 
