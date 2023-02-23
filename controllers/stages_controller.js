97fa37aac704cb6ea7d8bc7f300191f2bdbe92c8
jwong65 <jason.wong6507@gmail.com>
2023-02-23 10:49:22 -0500

    Fixed sytnax, newStage instead of newEvent

--- controllers/stages_controller.js
+++ controllers/stages_controller.js
@@ -18,7 +18,7 @@ stages.post('/', async(req, res)=>{
     const newStage = await Stage.create(req.body)
         res.status(200).json({
             message: 'Successfully added a new Stage',
-            data: newEvent
+            data: newStage
         })
     } catch(err) {
         res.status(500).json(err)
